46752eb11e9a7266897562e3376901a27e52b3ec
Jerry Lin <hqlyz112@163.com>
2018-10-02 07:19:23 -0700

    update

---
+++ syslim/t_sysconf.c
@@ -0,0 +1,27 @@
+#include "../lib/tlpi_hdr.h"
+
+static void sysconfPrint(const char *msg, int name) {
+    long lim;
+
+    errno = 0;
+    lim = sysconf(name);
+    if(lim != -1) {
+        printf("%s %ld\n", msg, lim);
+    } else {
+        if(errno == 0)
+            printf("%s (indeterminate)\n", msg);
+        else  
+            errExit("sysconf %s", msg);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    sysconfPrint("_SC_ARG_MAX:          ", _SC_ARG_MAX);
+    sysconfPrint("_SC_LOGIN_NAME_MAX:   ", _SC_LOGIN_NAME_MAX);
+    sysconfPrint("_SC_OPEN_MAX:         ", _SC_OPEN_MAX);
+    sysconfPrint("_SC_NGROUP_MAX:       ", _SC_NGROUPS_MAX);
+    sysconfPrint("_SC_PAGESIZE:         ", _SC_PAGESIZE);
+    sysconfPrint("_SC_RTSIG_MAX:        ", _SC_RTSIG_MAX);
+    return 0;
+}
