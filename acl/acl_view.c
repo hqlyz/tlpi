9ee08b1085dd064516ddd808f11dffb5d0429d1f
Jerry Lin <hqlyz112@163.com>
2018-10-09 08:07:16 -0700

    update

---
+++ acl/acl_view.c
@@ -0,0 +1,14 @@
+#include <linux/posix_acl.h>
+#include <linux/posix_acl_xattr.h>
+#include "../lib/tlpi_hdr.h"
+
+static void usageError(char *progName) {
+    fprintf(stderr, "Usage: %s [-d] filename\n", progName);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
