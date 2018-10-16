207d5ca667839f68e5686aa72654a1084e992d0c
Jerry Lin <hqlyz112@163.com>
2018-10-16 07:20:52 -0700

    update

--- timers/timed_read.c
+++ timers/timed_read.c
@@ -10,7 +10,32 @@ static void signalrmHandler(int sig) {
 
 int main(int argc, char const *argv[])
 {
+    struct sigaction sa;
+    char buf[BUF_SIZE];
+    ssize_t numRead;
+    int savedErrno;
+
     if(argc > 1 && strcmp(argv[0], "--help") == 0)
         usageErr("%s [num-secs [restart-flag]]\n", argv[0]);
+    sa.sa_flags = (argc > 2) ? SA_RESTART : 0;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_handler = signalrmHandler;
+    if(sigaction(SIGALRM, &sa, NULL) == -1)
+        errExit("sigaction");
+
+    alarm((argc > 1) ? getInt(argv[1], GN_NONNEG, "num-secs") : 10);
+    numRead = read(STDIN_FILENO, buf, BUF_SIZE - 1);
+    savedErrno = errno;
+    alarm(0);
+    errno = savedErrno;
+
+    if(numRead == -1) {
+        if(errno == EINTR)
+            printf("Read timed out\n");
+        else
+            errMsg("read");
+    } else {
+        printf("Successful read (%ld bytes): %.*s\n", (long)numRead, (int)numRead - 2, buf);
+    }
     return 0;
 }
